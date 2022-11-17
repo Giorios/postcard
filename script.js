7b2d1ce728b7e308ce92477c538d86336a3bda3f
Giorios <90624324+Giorios@users.noreply.github.com>
2022-11-17 13:33:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var e = document.getElementById("states");
+function onChange() {
+  var text = e.options[e.selectedIndex].text;
+    
+  document.getElementById("greeting").innerHTML = "Greetings from";
+  document.getElementById("state-heading").innerHTML = text; 
+  
+  var theme = e.options[e.selectedIndex].value;
+  document.getElementById("postcard").style.backgroundImage = "url(img/ak@2x.jpg)";
+}
+e.onchange = onChange;
+onChange();
+
